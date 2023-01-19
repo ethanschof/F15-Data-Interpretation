b793bf75956257e9355b1440a2dfad8dbddd464b
Ethan Schofield <ethanschof@gmail.com>
2023-01-19 09:45:50 -0700

    Initial Commit

---
+++ F15 Data Interpretation/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  F15 Data Interpretation
+//
+//  Created by Capstone 2023 on 1/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ F15 Data Interpretation/F15_Data_InterpretationApp.swift
@@ -0,0 +1,17 @@
+//
+//  F15_Data_InterpretationApp.swift
+//  F15 Data Interpretation
+//
+//  Created by Capstone 2023 on 1/19/23.
+//
+
+import SwiftUI
+
+@main
+struct F15_Data_InterpretationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
