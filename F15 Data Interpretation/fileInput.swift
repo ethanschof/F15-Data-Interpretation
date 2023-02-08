5130895a448b2638f84a2498f707c5487ecb2997
St4rCr0ss3d <104808255+St4rCr0ss3d@users.noreply.github.com>
2023-02-08 11:22:21 -0700

    bitInterpretation function

--- F15 Data Interpretation/fileInput.swift
+++ F15 Data Interpretation/fileInput.swift
@@ -12,6 +12,9 @@ class FileInput {
     var filename: String = "myChap10.ch10"
     var bytes = Data()
     
+    var curBitsRead = 0
+    
+    
     func FileInput(name: String) {
         self.filename = name
         do {
@@ -33,8 +36,64 @@ class FileInput {
         }
         
     }
+    s
+    func bytesToUInt64(byteArray: [UInt8], length : Int) -> UInt64 {
+        var total: UInt64 = 0
+        var power = 0.0
+        for byte in byteArray.reversed() {
+            for i in (0...7){
+                var mask = UInt64(byte & 1 << i) * UInt64(pow(2, power))
+                total = UInt64(total + mask)
+                power += 1
+            }
+        }
+        return total
+    }
     
-    func parseBytes() {
+    func bitInterpreter(numBits : Int, swapEndian : Bool) -> UInt64{
+        var numBytes = numBits / 8
+        var onlyBytes = true
+        if(numBits % 8 != 0){
+            numBytes += 1
+            onlyBytes = false
+        }
+        
+        var pulledBytes = [UInt8]()
+        
+        if(onlyBytes){
+            while(numBytes != 0){
+                pulledBytes.append(bytes.removeFirst())
+                numBytes -= 1
+            }
+        }else{
+            
+            while(numBytes != 1){
+                pulledBytes.append(bytes.removeFirst())
+                numBytes -= 1
+            }
+            
+            //remove unwanted bits
+            pulledBytes.append(bytes.first!)
+            pulledBytes[pulledBytes.lastIndex(of: pulledBytes.last!)!] = pulledBytes.last! >> (8 - numBits%8)
+            
+            if((curBitsRead + numBits%8)%8 == 0){
+                bytes.removeFirst()
+            }else{
+                //remove requested bits off the byte function
+                bytes[bytes.firstIndex(of: pulledBytes.first!)!] = pulledBytes.first! << (numBits%8)
+            }
+        }
+        
+        //swap endian of pulledBytes if true
+        if(swapEndian){
+            pulledBytes.reverse()
+        }
+        
+        var returnVal: UInt64 = bytesToUInt64(byteArray: pulledBytes, length: numBytes)
+        return returnVal
+    }
+    
+    func parseHeader() {
         
     }
 }
